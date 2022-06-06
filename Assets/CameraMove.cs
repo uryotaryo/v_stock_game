0b93d80e1910a0db633e376acf5e8bb61c883c64
RAZ-AKI-LAPTOP <akai55101124@icloud.com>
2022-06-06 12:23:19 +0900

    クリックによるステージboxの参照
    raycastを使用してboxの情報を取得
    TODO:ダウンしている間ずっと参照するから少し挙動をいじる

--- Assets/CameraMove.cs
+++ Assets/CameraMove.cs
@@ -16,8 +16,21 @@ public class CameraMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        GameObject hit_obj;
+        if(Input.GetMouseButton(0)){
+            
+            Ray ray = mycamera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit_info = new RaycastHit();
+            float max_distance = 100f;
+
+            bool is_hit = Physics.Raycast (ray, out hit_info, max_distance); 
+            if (is_hit) {
+                hit_obj = hit_info.transform.gameObject;
+                Debug.Log(hit_obj);
+            }   
+        }
+
         var mous = Input.mouseScrollDelta;
         mycamera.fieldOfView += mous.y * speed;
-        
     }
 }
