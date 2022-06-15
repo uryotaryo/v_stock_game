de6459b84abfee0685f273fbeaad96be8efc4bf7
RAZ-AKI-LAPTOP <akai55101124@icloud.com>
2022-06-15 11:21:14 +0900

    木の採集を繰り返せるようになった
    クールタイム
    木の採集約１０秒
    採集命令戻ってきてから約２秒

--- Assets/prot/prot_cont.cs
+++ Assets/prot/prot_cont.cs
@@ -12,10 +12,14 @@ public class prot_cont : MonoBehaviour
     private GameObject prot_tree;
     private bool returnbool = false;
     private float sleep = 10;
+    private float returnintbal = 2;
 
     // Start is called before the first frame update
     void Start()
     {
+        remove();
+    }
+    public void remove(){
         Vector3 pos;
         prot_npc.transform.position = GameManager.Get_Stage_Maneger().GetComponent<StageManeger>().VectorReturn(NPC_pos.x,NPC_pos.y);
         pos = prot_npc.transform.position;
@@ -38,6 +42,15 @@ public class prot_cont : MonoBehaviour
         sleep -= Time.deltaTime;
         if(sleep > 0)return;
         prot_npc.GetComponent<prot_NPC_cont>().target = NPC_pos;
+        if(prot_npc.GetComponent<prot_NPC_cont>().pos == NPC_pos){
+            returnintbal -= Time.deltaTime;
+            if(returnintbal > 0)return;
+            remove();
+            prot_NPC_cont.Move_Stop();
+            returnbool = false;
+            sleep = 10;
+            returnintbal = 2;
+        }
     }
     private void XZMove(Vector3 v){
         this.transform.position = new Vector3(v.x,1,v.z);
