95ea8e86a2be84d02387c77f9722f5bc9113ba1a
someyasomeya <pandahiirou@icloud.com>
2022-09-12 16:03:36 +0900

    射的屋のコミュニケーションを追加しました

--- Assets/Script/Conversation.cs
+++ Assets/Script/Conversation.cs
@@ -52,6 +52,31 @@ public class Conversation : MonoBehaviour
         Dict_Q.Add("会話3", new Question("趣味"));
         Dict_Q.Add("会話4", new Question("焼き鳥屋店主について"));
         Dict_Q.Add("会話5", new Question("悩み"));
+
+        //射的屋
+        Dict_Q.Add("射的:挨拶", new Question("こんにちは"));
+
+        Dict_Q.Add("射的:1", new Question("景品を集めてほしい"));
+
+        Dict_Q.Add("射的:2", new Question("プレイヤー行動選択肢"));
+        Dict_Q.Add("射的:2-1", new Question("射的返答1"));
+        Dict_Q.Add("射的:2-2", new Question("射的返答2"));
+        Dict_Q.Add("射的:2-3", new Question("射的返答3"));
+        Dict_Q.Add("射的:2-4", new Question("(もう一度頼もう)"));
+        Dict_Q.Add("射的:2-5", new Question("射的返答4"));
+        Dict_Q.Add("射的:2-6", new Question("射的返答5"));
+
+        Dict_Q.Add("射的:3", new Question("世間話"));
+        Dict_Q.Add("射的:3-1", new Question("嫌いなことの選択肢"));
+        Dict_Q.Add("射的:3-2", new Question("射的返答5"));
+        Dict_Q.Add("射的:3-2", new Question("射的返答6"));
+
+        Dict_Q.Add("会話1", new Question("好きなこと"));
+        Dict_Q.Add("会話2", new Question("嫌いなこと"));
+        Dict_Q.Add("会話3", new Question("目標"));
+        Dict_Q.Add("会話4", new Question("お面屋店主について"));
+        Dict_Q.Add("会話5", new Question("旅行に行くなら"));
+
     }
     /// <summary>
     /// 質問に紐づけされる解答一覧(N)
@@ -157,6 +182,61 @@ public class Conversation : MonoBehaviour
         Dict_Q["会話5"].Talks.Add("縁日で屋台の手伝いをお願いしたいのですが...");
         Dict_Q["会話5"].Talks.Add("大歓迎です！どんな手伝いがしたいんですか？");
         Dict_Q["会話5"].Talks.Add("甘い物を作る屋台などがあれば、手伝いたいですね。");
+
+        //射的屋
+        Dict_Q["射的:挨拶"].Anss.Add(new Reply("挨拶", "こんにちは！何の用事ですか？", Dict_Q["射的:1"]));
+
+        Dict_Q["射的:1"].Anss.Add(new Reply("要件", "何か用事があるんですか？", Dict_Q["射的:2"]));
+        Dict_Q["射的:1"].Anss.Add(new Reply("世間話", "何か用事があるんですか？", Dict_Q["射的:3"]));
+        Dict_Q["射的:1"].Anss.Add(new Reply("特になし", "了解です！", -2));
+
+        Dict_Q["射的:2"].Anss.Add(new Reply("丁寧", "今お時間大丈夫ですか？", Dict_Q["射的:2-1"]));
+        Dict_Q["射的:2"].Anss.Add(new Reply("一般", "頼みたいことがあります！", Dict_Q["射的:2-2"]));
+        Dict_Q["射的:2"].Anss.Add(new Reply("雑", "今暇？ちょっといい？？", Dict_Q["射的:2-3"]));
+
+        Dict_Q["射的:2-1"].Anss.Add(new Reply("", "大丈夫です！やりますよ", -1));
+        Dict_Q["射的:2-2"].Anss.Add(new Reply("", "わかりました！", 0));
+        Dict_Q["射的:2-3"].Anss.Add(new Reply("", "いいよー", Dict_Q["射的:2-4"]));
+
+        Dict_Q["射的:2-4"].Anss.Add(new Reply("一般", "頼みたいことがあるんですよね", Dict_Q["射的:2-5"]));
+        Dict_Q["射的:2-4"].Anss.Add(new Reply("雑", "ありがと！これやってもらえる？", Dict_Q["射的:2-6"]));
+
+        Dict_Q["射的:2-5"].Anss.Add(new Reply("", "おっけー！", 0));
+        Dict_Q["射的:2-6"].Anss.Add(new Reply("", "やっておくよ。", +1));
+
+        Dict_Q["射的:3"].Anss.Add(new Reply("Like", "好きなことは何ですか？", Dict_Q["射的:会話1"]));
+        Dict_Q["射的:3"].Anss.Add(new Reply("Hate", "嫌いなことは何ですか？", Dict_Q["射的:会話2"]));
+        Dict_Q["射的:3"].Anss.Add(new Reply("target", "目標はありますか？", Dict_Q["射的:会話3"]));
+        Dict_Q["射的:3"].Anss.Add(new Reply("Friends", "お面屋店主ってミステリアスですよね？", Dict_Q["射的:会話4"]));
+        Dict_Q["射的:3"].Anss.Add(new Reply("trip", "旅行に行きたい場所とかありますか？", Dict_Q["会話5"]));
+
+        Dict_Q["射的:3-1"].Anss.Add(new Reply("丁寧", "承知しました", Dict_Q["射的:会話3-2"]));
+        Dict_Q["射的:3-1"].Anss.Add(new Reply("一般", "これからは丁寧な言葉遣いを心がけていくよ", Dict_Q["射的:会話3-3"]));
+
+        Dict_Q["射的:3-2"].Anss.Add(new Reply("", "別に今は大丈夫だよw\n仕事や作業中は丁寧な言葉遣いを心がけてほしいかも！", -2));
+        Dict_Q["射的:3-3"].Anss.Add(new Reply("", "うん！それだとありがたいかも！", -2));
+
+        Dict_Q["射的:会話1"].Talks.Add("楽しいこと、盛り上がることは好きですね！");
+        Dict_Q["射的:会話1"].Talks.Add("じゃあお祭りも好きそうですね");
+        Dict_Q["射的:会話1"].Talks.Add("結構好きです！楽しみです！");
+
+        Dict_Q["射的:会話2"].Talks.Add("プライベートと仕事の切替が下手な人は苦手だったりするなぁ");
+        Dict_Q["射的:会話2"].Talks.Add("なるほど、もっと具体的に何かありますか？");
+        Dict_Q["射的:会話2"].Talks.Add("うーーん...。\n仕事や作業中は丁寧な言葉遣いを心がけてほしいかも！");
+        Dict_Q["射的:会話2"].Next_Question = Dict_Q["射的:3-1"];
+
+        Dict_Q["射的:会話3"].Talks.Add("強いて言うなら人を楽しませたいな！");
+        Dict_Q["射的:会話3"].Talks.Add("良い目標ですね！");
+        Dict_Q["射的:会話3"].Talks.Add("だよね！何かするならエンタメを選ぶ気がするなぁ");
+
+        Dict_Q["射的:会話4"].Talks.Add("僕の知る限り、話せば割と普通の人だよ！");
+        Dict_Q["射的:会話4"].Talks.Add("そうなんですね。\n顔が見えないので感情を読み取るのが難しいんです");
+        Dict_Q["射的:会話4"].Talks.Add("崩しすぎず硬すぎず、普通の距離感で話せばいいんじゃないかな！\nきっと仲良くなれると思うよ！");
+
+        Dict_Q["射的:会話5"].Talks.Add("銃を扱える国に行って銃を撃ってみたい！");
+        Dict_Q["射的:会話5"].Talks.Add("銃を撃つのってどんな感じなんだあろう...");
+        Dict_Q["射的:会話5"].Talks.Add("（詮索するのはやめておこう...）");
+
     }
     /// <summary>
     /// ゲーム内の全てのタスクをここで定義する 
@@ -168,6 +248,7 @@ public class Conversation : MonoBehaviour
         //例タスクはプログラム内で使う文字列同名だと上書きされる　最後はタスクの作業量
         All_Tasks["お面"] = new Task("お面", "お面を50個作ろう", 50);
         All_Tasks["綿あめ"] = new Task("綿あめ","ザラメを2000g用意しよう",2000);
+        All_Tasks["射的"] = new Task("射的", "射的の景品を10個用意しよう", 10);
     }
     /// <summary>
     /// 仮:)すべてのタスクから建物ごとに関係のあるタスクを振り分ける。
